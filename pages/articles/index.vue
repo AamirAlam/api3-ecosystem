be48e7e54273a202358275bcd04039727b519b64
beejsbj <burooj.bj@gmail.com>
2023-07-28 14:38:22 -0400

    Set layout for article grid

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -14,6 +14,6 @@ useServerSeoMeta({
 <template>
   <PageHeader heading="Articles/Blog" />
   <SectionColumn>
-    <ArticleGrid :layout="random" />
+    <ArticleGrid :layout="4" />
   </SectionColumn>
 </template>
