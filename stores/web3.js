cf43eecdcdf6da26d2ab38da1283277bad37e10b
= <aamiralam1991@gmail.com>
2023-07-30 22:36:03 +0530

    fix: production build crash

--- stores/web3.js
+++
@@ -1,9 +0,0 @@
-export const useWeb3Store = defineStore("web3", () => {
-  const web3 = useWeb3();
-
-  const isConnected = computed(() => web3.isConnected);
-
-  return {
-    isConnected,
-  };
-});
