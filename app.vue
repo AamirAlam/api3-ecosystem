0d1a7e02ca7c56bc04126a7d0045399c28ff4424
beejsbj <burooj.bj@gmail.com>
2023-07-30 17:23:02 -0400

    Move styling around

--- app.vue
+++ app.vue
@@ -33,3 +33,34 @@ onMounted(() => {
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style>
+html * {
+  --fk-font-family: var(--font);
+  --fk-font-size: 1rem;
+  --fk-font-weight: 400;
+  --fk-line-height: 1.4;
+  --fk-color-input: var(--ink);
+}
+form {
+  --multistep-radius: var(--corners);
+  --multistep-color-success: var(--color);
+  --fk-color-help: var(--ink);
+  fill: black !important;
+
+  /* vueform/multistep */
+
+  --ms-bg: var(--paper);
+  --ms-border-color: var(--ink);
+  --ms-radius: var(--corners);
+
+  --ms-tag-bg: var(--color);
+  --ms-tag-color: var(--paper);
+  --ms-tag-radius: var(--corners);
+
+  --ms-dropdown-bg: var(--paper);
+  --ms-dropdown-border-color: var(--ink);
+  --ms-dropdown-color: var(--ink);
+  --ms-dropdown-radius: var(--corners);
+}
+</style>
