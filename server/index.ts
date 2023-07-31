929dfcc951803693b5be46ffdb9a447e25a3d822
Aamir Alam <aamiralam1991@gmail.com>
2023-07-31 21:46:54 +0530

    add projects sync on startup (#53)
    * add projects sync on startup
    
    * fix: status message

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,19 @@
 import mongoose from "mongoose";
 import { Nitro } from "nitropack";
+import { syncProjectsToMongodb } from "./services/syncDb";
 
 export default async (_nitroApp: Nitro) => {
   const config = useRuntimeConfig();
 
   try {
     await mongoose.connect(config.mongodbURI);
+
+    // sync projects on server startup
     console.log("db connected:");
+
+    console.log("syncing projects");
+    await syncProjectsToMongodb();
+    console.log("projects synced");
   } catch (err) {
     console.log("mongodb connection error ", err);
   }
