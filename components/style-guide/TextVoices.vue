1f373576aed36664ed8b565913b58d7661e7da30
beejsbj <burooj.bj@gmail.com>
2023-08-04 21:49:50 -0400

    DElte type guide

--- components/style-guide/TextVoices.vue
+++ components/style-guide/TextVoices.vue
@@ -1,63 +1,104 @@
 <script setup>
+//convert to object
 const voices = ref([
-  "micro-voice",
-  "whisper-voice",
-  "calm-voice",
-  "notice-voice",
-  "firm-voice",
-  "attention-voice",
-  "teaser-voice",
-  "loud-voice",
-  "booming-voice",
+  {
+    name: "micro-voice",
+  },
+  {
+    name: "whisper-voice",
+  },
+  {
+    name: "calm-voice",
+  },
+  {
+    name: "notice-voice",
+  },
+  {
+    name: "firm-voice",
+  },
+  {
+    name: "attention-voice",
+  },
+  {
+    name: "teaser-voice",
+  },
+  {
+    name: "loud-voice",
+  },
+  {
+    name: "booming-voice",
+  },
 ]);
+
+function getFontSize() {
+  voices.value.forEach((voice) => {
+    const $voice = document.querySelector(`voice-card .${voice.name}`);
+    voice.fontSize = window.getComputedStyle($voice).fontSize;
+  });
+}
+
+onMounted(() => {
+  getFontSize();
+  window.addEventListener("resize", () => {
+    getFontSize();
+  });
+});
 </script>
 
 <template>
-  <h2 class="attention-voice">Text Voices</h2>
-  <ul>
-    <li v-for="voice in voices">
-      <voice-card>
-        <code>
-          {{ voice }}
-        </code>
-        <p :class="voice">This is a short sentence.</p>
-        <p :class="voice">this is a <em>regular emphasis</em> sentence.</p>
-        <p :class="voice">
-          This is a <strong>strong emphasis</strong> sentence.
-        </p>
-        <p :class="voice">
-          This is a
-          <strong><em>strong and regular emphasis</em></strong> sentence.
-        </p>
-        <p :class="voice">
-          This is a sentence with a <mark class="highlight">highlight </mark>.
-        </p>
-        <p :class="voice">
-          these <a href="?page=style-guide#"> are links </a> in the paragraph.
-        </p>
-        <p :class="voice" class="wrap">
-          This is a paragraph that should wrap to the next line. lorem ipsum
-          dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed
-          sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec
-          consectetur ante hendrerit.
-        </p>
-        <p :class="voice" class="wrap">
-          Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec
-          ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum dolor
-          sit amet, consectetur adipiscing elit. Aenean ut gravida lorem. Ut
-          turpis felis, pulvinar a semper sed, adipiscing id dolor. Pellentesque
-          auctor nisi id magna consequat sagittis. Curabitur dapibus enim sit
-          amet elit pharetra tincidunt feugiat nisl imperdiet.
-        </p>
-      </voice-card>
-    </li>
-  </ul>
+  <div class="text-voices">
+    <h2 class="attention-voice">Text Voices</h2>
+    <ul>
+      <li v-for="voice in voices">
+        <voice-card>
+          <code>
+            {{ voice.name }}
+          </code>
+          <p :class="voice.name">The font family is Inter.</p>
+          <p :class="voice.name">
+            The font size is <code>{{ voice.fontSize }}</code>
+          </p>
+
+          <p :class="voice.name">
+            this is a <em>regular emphasis</em> sentence.
+          </p>
+          <p :class="voice.name">
+            This is a <strong>strong emphasis</strong> sentence.
+          </p>
+          <p :class="voice.name">
+            This is a
+            <strong><em>strong and regular emphasis</em></strong> sentence.
+          </p>
+          <p :class="voice.name">
+            This is a sentence with a <mark class="highlight">highlight </mark>.
+          </p>
+          <p :class="voice.name">
+            these <a href="?page=style-guide#"> are links </a> in the paragraph.
+          </p>
+          <p :class="voice.name" class="wrap">
+            This is a paragraph that should wrap to the next line. lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Donec a diam lectus.
+            Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra
+            nec consectetur ante hendrerit.
+          </p>
+          <p :class="voice.name" class="wrap">
+            Nam tincidunt congue enim, ut porta lorem lacinia consectetur. Donec
+            ut libero sed arcu vehicula ultricies a non tortor. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Aenean ut gravida
+            lorem. Ut turpis felis, pulvinar a semper sed, adipiscing id dolor.
+            Pellentesque auctor nisi id magna consequat sagittis. Curabitur
+            dapibus enim sit amet elit pharetra tincidunt feugiat nisl
+            imperdiet.
+          </p>
+        </voice-card>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped lang="scss">
 ul {
   display: grid;
-  gap: 3rem;
   margin-top: 1rem;
 
   li + li {
