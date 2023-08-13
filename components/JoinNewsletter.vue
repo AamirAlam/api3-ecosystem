e2dbc0cfdb909b300f9793cb69269e32fc0c490b
beejsbj <burooj.bj@gmail.com>
2023-08-13 18:05:52 -0400

    Create Join newsletter component

---
+++ components/JoinNewsletter.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { setErrors, FormKitMessages } from "@formkit/vue";
+import { getValidationMessages } from "@formkit/validation";
+
+function submitHandler(event) {
+  console.log("submitHandler", event);
+}
+</script>
+
+<template>
+  <div class="join-newsletter">
+    <text-content>
+      <h3 class="attention-voice gradient-text-color">Join our Newsletter</h3>
+      <p>Get the latest news and updates from the API3 ecosystem.</p>
+      <FormKit type="form" id="join-newsletter" :actions="false" v-auto-animate>
+        <FormKit
+          type="email"
+          placeholder="johndoe@email.com"
+          id="email-newsletter"
+          name="email"
+          validation="required|email"
+          v-auto-animate
+        />
+        <FormKit
+          type="submit"
+          label="Join"
+          input-class="$reset button calm-voice"
+        />
+      </FormKit>
+    </text-content>
+    <graphic-content>
+      <LoadingSpinner />
+    </graphic-content>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.join-newsletter {
+  border-radius: var(--corners);
+  position: relative;
+  overflow: hidden;
+  //   box-shadow: var(--shadow);
+
+  text-content {
+    padding: 2rem;
+  }
+}
+p {
+  margin-top: 0.5rem;
+}
+form {
+  margin-top: 2rem;
+  justify-items: start;
+}
+
+graphic-content {
+  position: absolute;
+  inset: 0;
+  top: -50%;
+  left: 25%;
+  z-index: -1;
+
+  :deep(.loading-spinner) {
+    width: 100%;
+  }
+}
+</style>
