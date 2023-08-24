c928cec8353db5bdcfdeb4a20e1c008c324a208a
beejsbj <burooj.bj@gmail.com>
2023-08-24 14:28:15 -0400

    Fix menu icon

--- components/icons/MenuIcon.vue
+++ components/icons/MenuIcon.vue
@@ -6,23 +6,18 @@
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path
-      style="stroke-width: 8px"
-      d="M11 12H29"
-      stroke="var(--ink)"
-      stroke-linecap="round"
-    />
-    <path
-      style="stroke-width: 8px"
-      d="M11 20H29"
-      stroke="var(--ink)"
-      stroke-linecap="round"
-    />
-    <path
-      style="stroke-width: 8px"
-      d="M11 28H29"
-      stroke="var(--ink)"
-      stroke-linecap="round"
-    />
+    <path d="M11 12H29" stroke="var(--ink)" stroke-linecap="round" />
+    <path d="M11 20H29" stroke="var(--ink)" stroke-linecap="round" />
+    <path d="M11 28H29" stroke="var(--ink)" stroke-linecap="round" />
   </svg>
 </template>
+
+<style scoped lang="scss">
+path {
+  stroke-width: 2px;
+
+  @media (min-width: 768px) {
+    stroke-width: 8px;
+  }
+}
+</style>
