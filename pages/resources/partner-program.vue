8baf6c907d2a4197f861145c93ed9c008f68d976
beejsbj <burooj.bj@gmail.com>
2024-01-04 19:18:19 -0500

    init page

---
+++ pages/resources/partner-program.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+  <SectionColumn class="hero">
+    <h1 class="booming-voice gradient-text">Become an API3 Partner</h1>
+
+    <picture class="hero-image">
+      <img src="/images/icons/dao.svg" alt="" />
+    </picture>
+  </SectionColumn>
+
+  <SectionColumn class="about">
+    <h2 class="atention-voice">About the Program</h2>
+
+    <p class="notice-voice">
+      At the heart of API3's success is a collaborative network of contributors,
+      clients, and influencers, each playing a pivotal role in expanding the
+      reach and impact of our Oracle Stack. API3 DAO seeks to incentivise growth
+      of dAPIs through the introduction of a referral scheme. This programme
+      rewards affiliates for successfully introducing a DeFi protocol that leads
+      to an integration of a dAPI price feed.
+    </p>
+  </SectionColumn>
+</template>
+
+<style scoped lang="scss"></style>
